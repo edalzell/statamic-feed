254fe49e947ab5202df1eaf98b978b248ffd7886
Erin Dalzell <erin@emdMB.local>
2017-12-06 12:32:02 -0800

    Routes now pull from config, and, actually work.

--- Feed/FeedServiceProvider.php
+++ Feed/FeedServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Statamic\Addons\Feed;
 
+use Statamic\API\Str;
 use Illuminate\Routing\Route;
 use Statamic\Extend\ServiceProvider;
 use Illuminate\Routing\RouteCollection;
@@ -27,13 +28,18 @@ class FeedServiceProvider extends ServiceProvider
         $new_routes = new RouteCollection();
         foreach ($old_routes as $i => $route) {
             if ($route->getUri() == '{segments?}') {
-                $json_feed_route = new Route(['GET'], 'feed.json', ['uses' => '\Statamic\Addons\Feed\FeedController@json']);
+                $json_feed_route = new Route(['GET'],
+                                             Str::removeLeft($this->getConfig('json_url','feed.json'), '/'),
+                                             ['uses' => '\Statamic\Addons\Feed\FeedController@json']);
                 $new_routes->add($json_feed_route);
-                $atom_feed_route = new Route(['GET'], 'feed', ['uses' => '\Statamic\Addons\Feed\FeedController@atom']);
+                $atom_feed_route = new Route(['GET'],
+                                             Str::removeLeft($this->getConfig('atom_url','feed'), '/'),
+                                             ['uses' => '\Statamic\Addons\Feed\FeedController@atom']);
                 $new_routes->add($atom_feed_route);
             }
             $new_routes->add($route);
         }
+
         app('router')->setRoutes($new_routes);
     }
 }
